d5fe7492a67db23fd4d1527cf42ae8bf55ee3dbd
redox <a.anandfns@gmail.com>
2025-08-05 22:08:38 +0530

    Reverse a Number

---
+++ LC7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int sum = 0;
+        while(x != 0) {
+            int digit = x % 10;
+            if(sum > INT_MAX/10 || sum < INT_MIN/10) return 0; // To check if the sum is overflowing or not from its limit
+            sum = sum * 10 + digit;
+            x /= 10;
+        }
+
+       return sum; 
+    }
+};
