22881795ab20fa64bf93f74dff65a176dc9fea6b
redox <a.anandfns@gmail.com>
2025-08-06 09:27:50 +0530

    Two Sum

--- LC1.cpp
+++ LC1.cpp
@@ -16,3 +16,26 @@ public:
         return {-1, -1};
     }
 };
+
+//********************Using Unordered Map********************
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m; // Creating empty for - will store key ~ value of second num | value ~ its index
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+            int first = nums[i];
+            int second = target - first;
+
+            if(m.find(second) != m.end()) { // if the other pair w.r.t. first is found, then the indices will get returned
+                return {i , m[second]};     // at first the map will be empty, so it will store the pair of fisrt
+            } else { // otherwise the pair's value as key along with its index as value, get inserted into the map as a key , value pair
+                m.insert({first , i});
+            }
+        }
+
+        return {-1, -1};
+    }
+};
