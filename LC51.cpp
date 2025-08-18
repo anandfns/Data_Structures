f62d3e87b80693c0a737fc24d67bf174e12306dc
redox <a.anandfns@gmail.com>
2025-08-18 18:08:25 +0530

    N-Queens

---
+++ LC51.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isSafe(vector<string>& board, int n, int row, int col) {
+        //horizontal
+        for(int i=0; i<n; i++) {
+            if(board[row][i] == 'Q') return false;
+        }
+
+        //vertical
+        for(int i=0; i<n; i++) {
+            if(board[i][col] == 'Q') return false;
+        }
+
+        //left diagonal
+        for(int i=row, j=col; i>=0 && j>=0; i--, j--){
+            if(board[i][j] == 'Q') return false;
+        }
+
+        //right diagonal
+        for(int i=row, j=col; i>=0 && j<n; i--, j++) {
+            if(board[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+    void nQueens(vector<string>& board, int n, int row, vector<vector<string>>& ans) {
+        if(row == n) {
+            ans.push_back({board});
+            return;
+        }
+
+        for(int j=0; j<n; j++) {
+            if(isSafe(board, n, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, n, row + 1, ans);
+                board[row][j] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> ans;
+        nQueens(board, n, 0, ans);
+
+        return ans;
+    }
+};
