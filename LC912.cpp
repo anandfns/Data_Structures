5a6c8cddbf5be46a575aea2a223203a3b90acecd
redox <a.anandfns@gmail.com>
2025-09-18 23:06:37 +0530

    Sort an Array

--- LC912.cpp
+++ LC912.cpp
@@ -1,3 +1,4 @@
+// Merge Sort ~ TC : O(nlogn) ~ SC : O(n)
 class Solution {
 public:
     void merge(vector<int>&nums, int low, int mid, int high) {
@@ -40,6 +41,38 @@ public:
     }
     vector<int> sortArray(vector<int>& nums) {
         mergeSort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
+
+// Quick Sort ~ TC : O(nlogn) ~ SC : O(1) we are not considering the recursion call stack
+class Solution {
+public:
+    int partition(vector<int>& nums, int low, int high) {
+        int pivot = nums[low];
+        int i = low;
+        int j = high;
+
+        while(i < j) {
+            while(nums[i] <= pivot && i <= high-1) i++;
+            while(nums[j] > pivot && j >= low+1) j--;
+            if(i < j) swap(nums[i], nums[j]);
+        }
+
+        swap(nums[low], nums[j]);
+        return j;
+    }
+
+    void quickSort(vector<int>& nums, int low, int high) {
+        if(low < high) {
+            int PI = partition(nums, low, high);
+            quickSort(nums, low, PI - 1);
+            quickSort(nums, PI + 1, high);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums, 0, nums.size()-1);
         return nums;
     }
 };
