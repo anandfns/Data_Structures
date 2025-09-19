71338438ec14a66e9d21ccdb217d99947f5bd221
redox <a.anandfns@gmail.com>
2025-09-19 22:09:31 +0530

    Combination sum I

---
+++ LC39.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void func(vector<int>& can, int tar, vector<vector<int>>& ans, vector<int>& temp, int idx){
+        if(tar == 0) {
+            ans.push_back(temp);
+            return;
+        }
+        if(idx == can.size()) {
+            if(tar == 0) {
+                ans.push_back(temp);
+            }
+            return;
+        }
+
+        if(can[idx] <= tar) {
+            temp.push_back(can[idx]);
+            func(can, tar - can[idx], ans, temp, idx);
+            temp.pop_back();
+        }
+
+        func(can, tar, ans, temp, idx+1);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& can, int tar) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        func(can, tar, ans, temp, 0);
+        return ans;
+
+    }
+};
