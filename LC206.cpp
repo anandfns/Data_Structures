e75ee039bbc36003271613d9bd7324a4bc6e236d
redox <a.anandfns@gmail.com>
2025-09-23 16:30:16 +0530

    Reverse a Linked List

--- LC206.cpp
+++ LC206.cpp
@@ -18,3 +18,19 @@ public:
         return back;
     }
 };
+
+// Recursive approach ~ TC : O(n)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // or we can make a helper function to do the same by passing only head
+        if(head == nullptr || head->next == nullptr) return head;
+
+        ListNode* newHead = reverseList(head->next);
+
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+};
