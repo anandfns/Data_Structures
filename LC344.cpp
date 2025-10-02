56e5921a31da048f42e621d27cb75372561f9bb0
redox <a.anandfns@gmail.com>
2025-10-02 10:39:40 +0530

    Reverse String

---
+++ LC344.cpp
@@ -0,0 +1,20 @@
+// Using in-Built Function
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        return reverse(s.begin(), s.end());
+    }
+};
+
+// BY Swapping
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int low = 0;
+        int high = (int)s.size()-1;
+
+        while(low < high) {
+            swap(s[low++], s[high--]);
+        }
+    }
+};
