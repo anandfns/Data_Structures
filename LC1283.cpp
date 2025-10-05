ef1faaa2b00285910614b218ac4afd1db9d97a87
redox <a.anandfns@gmail.com>
2025-10-05 19:40:50 +0530

    Find the Smallest Divisor Given a Threshold

---
+++ LC1283.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool check(vector<int>& nums, int t, int mid) {
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++) {
+            sum += ceil( (double)nums[i] / (double)mid );
+        }
+        return sum <= t;
+    }
+
+    int maxElem(vector<int>& nums) {
+        int ans = INT_MIN;
+        for(int i=0; i<nums.size(); i++) {
+            ans = max(nums[i], ans);
+        }
+        return ans;
+    }
+
+    int smallestDivisor(vector<int>& nums, int t) {
+        int low = 1;
+        int high = maxElem(nums);
+        // int ans = high;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
+            if(check(nums, t, mid)) {
+                // ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
