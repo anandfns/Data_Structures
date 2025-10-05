5a8427e4f1f4628d45e6a34a8c09f38cd974bafc
redox <a.anandfns@gmail.com>
2025-10-05 19:43:41 +0530

    Set Matrix to Zero

---
+++ LC73.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void helper(vector<vector<int>>& matrix, pair<int, int> p) {
+        // for hri
+        for(int i=0; i<matrix[p.first].size(); i++) {
+            matrix[p.first][i] = 0;
+        }
+        
+        // for ver
+        for(int i=0; i<matrix.size(); i++) {
+            matrix[i][p.second] = 0;
+        }
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int, int>> cords;
+        for(int i=0; i<matrix.size(); i++) {
+            for(int j=0 ; j<matrix[i].size(); j++) {
+                if(matrix[i][j] == 0) {
+                    cords.push_back({i,j});
+                }
+            }
+        }
+
+        for(int i=0; i<cords.size(); i++) {
+            helper(matrix, cords[i]);
+        }
+    }
+};
