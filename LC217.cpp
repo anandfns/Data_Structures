c1a3436310a0dc45cf67914dd5c03c88d63fea6e
redox <a.anandfns@gmail.com>
2025-10-10 00:06:52 +0530

    Contains Duplicate

---
+++ LC217.cpp
@@ -0,0 +1,23 @@
+// Using HashMap ~ TC : O(N) ans SC : O(N)
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++) {
+            if(i > 0 && nums[i] == nums[i-1]) return true;
+        }
+        return false;
+    }
+};
+
+// Using sort ~ TC : O(NlogN)
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++) {
+            if(i > 0 && nums[i] == nums[i-1]) return true;
+        }
+        return false;
+    }
+};
