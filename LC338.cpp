434aee9cf34e2c892fd2f074391b5a5329971c32
redox <a.anandfns@gmail.com>
2025-10-20 23:42:42 +0530

    Counting Bits

---
+++ LC338.cpp
@@ -0,0 +1,18 @@
+// Resolve
+// TC : O(NlogN)
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0; i<=n; i++) {
+            int cnt = 0;
+            int num = i;
+            while(num != 0) {
+                if(num&1) cnt++;
+                num = num >> 1;
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
