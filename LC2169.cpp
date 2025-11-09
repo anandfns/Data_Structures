c9de287dac2f6c8a7a04b932558f9932f9b7e7c2
redox <a.anandfns@gmail.com>
2025-11-10 00:23:10 +0530

    Count Operations to Obtain Zero

---
+++ LC2169.cpp
@@ -0,0 +1,31 @@
+// TC : 𝑂(log(min(a,b)))
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int cnt = 0;
+        while(num1 != 0 && num2 != 0) {
+            if(num1 > num2) {
+                cnt += num1/num2;
+                num1 = num1 % num2;
+            } else {
+                cnt += num2/num1;
+                num2 = num2 % num1;
+            }
+        }
+        return cnt;
+    }
+};
+
+// Equivalent but slower
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int cnt = 0;
+        while(num1 != 0 && num2 != 0) {
+            if(num1 > num2) num1 = num1 - num2;
+            else num2 = num2 - num1;
+            cnt++;
+        }
+        return cnt;
+    }
+};
